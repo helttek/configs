0ca13bd228c20e83e424dcdb371f2dba3c765fe5
anton <kasgotor3@gmail.com>
2026-03-04 21:56:47 +0700

    added highlighting for treesitter

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -2,6 +2,21 @@ return {
 	{
 		'nvim-treesitter/nvim-treesitter',
 		lazy = false,
-		build = ':TSUpdate'
+		build = ':TSUpdate',
+		config = function()
+			require("nvim-treesitter").setup({
+				ensure_installed = {
+					"lua",
+					"c",
+					"cpp",
+					"java",
+					"python"
+				},
+				auto_install = true,
+				highlight = {
+					enable = true,
+				},
+			})
+		end,
 	}
 }
